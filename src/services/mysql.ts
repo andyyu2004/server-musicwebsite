7317a0f7d57b3084b54c4eae816d0efd5e5901bc
Andy <andyyu2004@gmail.com>
2019-01-07 22:40:27 +1300

    added conditional for RDS / local

--- src/services/mysql.ts
+++ src/services/mysql.ts
@@ -1,23 +1,27 @@
 import * as mysql from 'mysql';
 
 // mysql -u RDS_username -p -h RDS_endpoint
+let pool;
 
-const pool = mysql.createPool({
-  connectionLimit: 10, 
-  host: process.env.MYSQL_HOSTNAME,
-  user: process.env.MYSQL_USERNAME,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-});
+if (process.env.USE_RDS) {
+  pool = mysql.createPool({
+    connectionLimit: 10, 
+    host: process.env.MYSQL_HOSTNAME,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  });
+} else {
+  pool = mysql.createPool({
+    connectionLimit: 10, 
+    host: process.env.RDS_HOSTNAME,
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    port: 3306,
+    database: process.env.MYSQL_DATABASE,
+  });
+}
 
-// const pool = mysql.createPool({
-//   connectionLimit: 10, 
-//   host: process.env.RDS_HOSTNAME,
-//   user: process.env.RDS_USERNAME,
-//   password: process.env.RDS_PASSWORD,
-//   port: 3306,
-//   database: process.env.MYSQL_DATABASE,
-// });
 
 pool.on('connection', conn => console.log("Mysql connected"));
 
